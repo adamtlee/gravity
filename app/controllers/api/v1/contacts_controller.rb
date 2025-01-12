5d74429a05d4ba5d02452b2a4efe73a5b9bc7aad
adamtlee <adam.lee346@gmail.com>
2025-01-12 12:53:33 -0700

    Added API for contacts

---
+++ app/controllers/api/v1/contacts_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::ContactsController < ApplicationController
+    before_action :set_contact, only: [:show, :update, :destroy]
+  
+    # GET /api/v1/contacts
+    def index
+      @contacts = Contact.all
+      render json: JSON.pretty_generate(@contacts.as_json)
+    end
+  
+    # GET /api/v1/contacts/:id
+    def show
+      render json: @contact
+    end
+  
+    # POST /api/v1/contacts
+    def create
+      @contact = Contact.new(contact_params)
+      if @contact.save
+        render json: @contact, status: :created, location: api_v1_contact_url(@contact)
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /api/v1/contacts/:id
+    def update
+      if @contact.update(contact_params)
+        render json: @contact
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /api/v1/contacts/:id
+    def destroy
+      @contact.destroy
+    end
+  
+    private
+  
+    def set_contact
+      @contact = Contact.find(params[:id])
+    end
+  
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :city, :state, :country)
+    end
+  end
+  
